f37d38a1435c379aa0335d2f893980c74df5809c
Nikita Khramushin <beachsidebearhi@gmail.com>
2022-01-27 03:58:32 +0700

    Fixes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import './Header.scss';
 const Header = () => {
     return(
         <div className={'Header'}>
-            <Link to={'/'} className={'Header__title'}>Сад-огород-ебать-в-рот</Link>
+            <Link to={'/'} className={'Header__title'}>Sad Ogorod</Link>
             <div>
             <Link to={'/recipe/add'} className="Header__modules">Добавить рецепт</Link>
             <Link to={'/modules'} className={'Header__modules'}>Модули</Link>
